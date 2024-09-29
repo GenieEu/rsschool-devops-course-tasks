a0031f23552bf04002abd2765840ee306826d5e5
Maria Park <maryeugepark@gmail.com>
2024-09-30 05:23:19 +0600

    Initial commit

---
+++ terraform/03-iamrole.tf
@@ -0,0 +1,29 @@
+resource "aws_iam_role" "github_actions_role" {
+  name = "GithubActionsRole"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Principal = {
+          Federated = aws_iam_openid_connect_provider.github_actions.arn
+        },
+        Action = "sts:AssumeRoleWithWebIdentity",
+        Condition = {
+          StringEquals = {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com",
+            "token.actions.githubusercontent.com:sub" = "repo:GenieEu/rsschool-devops-course-tasks:ref:refs/heads/main"
+          }
+        }
+      }
+    ]
+  })
+
+  depends_on = [aws_iam_openid_connect_provider.github_actions]
+}
+
+resource "aws_iam_role_policy_attachment" "role_policies" {
+  role       = aws_iam_role.github_actions_role.name
+  for_each   = toset(var.iam-role-policies)
+  policy_arn = each.value
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,20 @@
+variable "region" {
+  type        = string
+  default     = "us-east-1"
+  description = "An AWS region for infrastructure deployment"
+}
+
+variable "iam-role-policies" {
+  type = list(string)
+  default = [
+    "arn:aws:iam::aws:policy/AmazonEC2FullAccess",
+    "arn:aws:iam::aws:policy/AmazonRoute53FullAccess",
+    "arn:aws:iam::aws:policy/AmazonS3FullAccess",
+    "arn:aws:iam::aws:policy/AmazonDynamoDBFullAccess",
+    "arn:aws:iam::aws:policy/IAMFullAccess",
+    "arn:aws:iam::aws:policy/AmazonVPCFullAccess",
+    "arn:aws:iam::aws:policy/AmazonSQSFullAccess",
+    "arn:aws:iam::aws:policy/AmazonEventBridgeFullAccess"
+  ]
+  description = "A list of policies to attach to the GitHubActionsRole"
+}
